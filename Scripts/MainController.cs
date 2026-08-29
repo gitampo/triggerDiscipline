a945be88cf553573bca18ce8392449842884f61e
Emanuele <118837721+imellag@users.noreply.github.com>
2026-08-29 09:49:58 +0200

    Creato target buono, aggiunti colori per testare

--- Scripts/MainController.cs
+++ Scripts/MainController.cs
@@ -10,6 +10,8 @@ Script da usare per controllare lo stato generale del gioco:
 */
 public partial class MainController : Node
 {
+	public float punteggio = 0;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
