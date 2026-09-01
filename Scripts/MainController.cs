fe297b12de873ed41dde654b58ddb19968c0acec
Emanuele <118837721+imellag@users.noreply.github.com>
2026-09-01 17:50:58 +0200

    Iniziato menu di pausa

--- Scripts/MainController.cs
+++ Scripts/MainController.cs
@@ -10,15 +10,48 @@ Script da usare per controllare lo stato generale del gioco:
 */
 public partial class MainController : Node
 {
-	public float punteggio = 0;
+	public float score = 0;
+	private bool isGameRunning = false;
+	[Export] private Button _continueButton;
+	[Export] private Button _exitButton;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_continueButton.Pressed += () => HandleButtonClick("Continue");
+		_exitButton.Pressed += () => HandleButtonClick("Exit");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
+	}
+
+	private void HandleButtonClick(string action)
+	{
+		if (isGameRunning)
+		{
+			if (action == "Continue")
+			{
+
+			}
+			if (action == "Exit")
+			{
+				isGameRunning = false;
+				score = 0;
+				GetTree().ChangeSceneToFile("res://scenes/menu.tscn");
+			}
+		}
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		// per vedere la lista degli eventi esistenti/aggiungerne nuovi:
+		// progetto -> impostazioni del progetto -> mappa di input
+		if (@event.IsActionPressed("ui_cancel"))
+		{
+			GD.Print("esc");
+		}
 	}
 }
